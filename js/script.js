4514e8a0416df18c21f5e2caab76d71036c95b63
DmKhanin <dmkhanin@users.noreply.github.com>
2017-11-06 23:32:58 +0300

    Change game logic
    add collision
    add map

--- js/script.js
+++ js/script.js
@@ -2,54 +2,123 @@ $(document).ready(function () {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     var animation = 0;
-    document.addEventListener("keydown",control);
+    document.addEventListener("keydown", control);
+    var next_ind_x=16;
+    var next_ind_y=21;
+    var save_position_x = 410;
+    var save_position_y = 310;
+    var q;
+    var e=0;
+    var map = [ //15 20 
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 20  0
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 40  1
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1], // 60  2
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 80  3
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1], //100  4
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //120  5
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //140  6
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //160  7
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //180  8
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //200  9
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //220 10
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //240 11
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //260 12
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //280 13
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //300 14
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //320 15
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //340 16
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //360 17
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //380 18
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //400 19
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //420 20
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //440 21
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //460 22
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //480 23
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //500 24
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1], //520 25
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //540 26
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1], //560 27
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], //580 28
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  //600 29
+    ];
+
     var packman = {
-        positionX: 400,
-        positionY: 300,
-        rotation: 0
+        positionX: 410,
+        positionY: 310,
+        rotation: 0,
+        currentIndX: 20,
+        currentIndY: 15,
+        speed: 4,
+        save_position_x:390,
+        save_position_y: 290
     };
 
     function control() {
         switch (String.fromCharCode(event.keyCode)) {
             case "A":
-                packman.rotation = 180;
+                packman.rotation = 180; left();
                 break;
             case "W":
                 packman.rotation = 270;
                 break;
             case "D":
-                packman.rotation = 0;
+                packman.rotation = 0; right();
                 break;
             case "S":
                 packman.rotation = 90;
                 break;
         }
     }
+    /*if (map[i+1][j]==1){
+        ctx.fillRect(j*40+10,i*40-20,20,80);
+    }*/
+    function drawMap() {
+        for (var i = 0; i < 30; i++) {
+            for (var j = 0; j < 40; j++) {
+                if (map[i][j] == 1) {
+                    ctx.fillStyle = "Blue"; //цвет заливки
+                    ctx.beginPath(); //начинаем рисование
+                    ctx.fillRect(j * 20, i * 20, 20, 20);
+                    ctx.closePath(); // завершаем рисование
+                    ctx.fill(); //заливаем
+                }
+            }
+        }
+
+    }
     function game() {
+        console.warn(packman.positionY);
         ctx.clearRect(0, 0, 800, 600);
+        drawMap();
+        e++;
+        switch (packman.rotation){
+            case 0: right(); break;
+            case 180: left(); break;
+            case 90:  bottom(); break;
+            case 270: top(); break;
+        }
         if (animation >= 0 && animation <= 8) {
             switch (packman.rotation) {
-                case 0: drawPackman(ctx, packman.positionX, packman.positionY, 20, (Math.PI) / 6, 5.7); break;
-                case 90: drawPackman(ctx, packman.positionX, packman.positionY, 20, (Math.PI*2) / 3, 7.3); break;
-                case 180: drawPackman(ctx, packman.positionX, packman.positionY, 20, (Math.PI * 7) / 6, 9); break;
-                case 270: drawPackman(ctx, packman.positionX, packman.positionY, 20, (Math.PI*5) / 3, 10.5); break;
+                case 0: drawPackman(ctx, packman.positionX, packman.positionY, 10, (Math.PI) / 6, 5.7); break;
+                case 90: drawPackman(ctx, packman.positionX, packman.positionY, 10, (Math.PI * 2) / 3, 7.3); break;
+                case 180: drawPackman(ctx, packman.positionX, packman.positionY, 10, (Math.PI * 7) / 6, 9); break;
+                case 270: drawPackman(ctx, packman.positionX, packman.positionY, 10, (Math.PI * 5) / 3, 10.5); break;
                 default: break;
             }
             animation++;
         }
         if (animation >= 8 && animation <= 16) {
-
-            drawPackman(ctx, packman.positionX, packman.positionY, 20, (Math.PI * 7) / 6, 10);
+            drawPackman(ctx, packman.positionX, packman.positionY, 10, (Math.PI * 7) / 6, 10);
             animation++
             if (animation == 16) {
                 animation = 0;
             };
         }
         switch (packman.rotation) {
-            case 0: packman.positionX += 4; break;
-            case 90: packman.positionY +=4; break;
-            case 180: packman.positionX -= 4; break;
-            case 270: packman.positionY -=4; break;
+            case 0: packman.positionX += packman.speed; break;
+            case 90: packman.positionY += packman.speed; break;
+            case 180: packman.positionX -= packman.speed; break;
+            case 270: packman.positionY -= packman.speed; break;
             default: break;
         }
         if (packman.positionX <= 20) {
@@ -83,10 +152,105 @@ $(document).ready(function () {
     }
 
 
-    setInterval(game, 1000 / 15);
+    setInterval(game, 60);
 
-    $("#hide").click(function(){
+    $("#hide").click(function () {
         document.getElementById("help").style.display = "none";
     });
 
+    function top (){
+        if (packman.positionY+20 == packman.save_position_y){
+            switch (packman.rotation) {
+                case 0: packman.currentIndX += 1; next_ind_x = packman.currentIndX + 1; break;
+                case 90: packman.currentIndY += 1; next_ind_y = packman.currentIndY + 1; break;
+                case 180: packman.currentIndX -= 1; next_ind_x = packman.currentIndX - 1; break;
+                case 270: packman.currentIndY -= 1; next_ind_y = packman.currentIndY - 1; break;
+            }
+            e=0;
+            console.log(next_ind_y + " " + next_ind_x );
+            
+            if (map[next_ind_y-1][next_ind_x]==1){
+                packman.speed = 0;
+                console.log("STOP! " + packman.speed)
+            }
+            packman.save_position_x = packman.positionX;
+            packman.save_position_y = packman.positionY;
+            console.log(packman.currentIndY + " " +packman.currentIndX);
+        }
+    }
+    function bottom (){
+        if (packman.positionY-20 == packman.save_position_y){
+
+            packman.currentIndY += 1; next_ind_y = packman.currentIndY + 1;
+            e=0;
+            console.log(next_ind_y + " " + next_ind_x );
+            
+            if (map[next_ind_y-1][next_ind_x-1]==1){
+                packman.speed = 0;
+                console.log("STOP! " + packman.speed)
+            }
+            packman.save_position_y = packman.positionY;
+            console.log(packman.currentIndY + " " +packman.currentIndX);
+        }
+    }
+    function right (){
+        if (packman.positionX-20 == packman.save_position_x){
+               packman.currentIndX += 1; next_ind_x = packman.currentIndX + 1;
+            e=0;
+            console.log(next_ind_y + " " + next_ind_x );
+            
+            if (map[next_ind_y][next_ind_x]==1){
+                packman.speed = 0;
+                console.log("STOP! " + packman.speed)
+            }
+            packman.save_position_x = packman.positionX;
+            console.log(packman.currentIndY + " " +packman.currentIndX);
+        }
+    }
+    function left (){
+        if (packman.positionX+20 == packman.save_position_x){
+               packman.currentIndX -= 1; next_ind_x = packman.currentIndX - 1;
+            e=0;
+            console.log(next_ind_y + " " + next_ind_x );
+            
+            if (map[next_ind_y-1][next_ind_x-1]==1){
+                packman.speed = 0;
+                console.log("STOP! " + packman.speed)
+            }
+            packman.save_position_x = packman.positionX;
+            console.log(packman.currentIndY + " " +packman.currentIndX);
+        }
+    }
 })
+/*
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+ */
